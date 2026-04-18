6917a8b5bd3dc45ffd79a50985261209b225aab2
sokome7 <136597157+sokome7@users.noreply.github.com>
2026-04-18 10:55:16 -0400

    updated splashscreen: slowed down fade out

--- nodoze/components/SplashScreen.tsx
+++ nodoze/components/SplashScreen.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
 import { Image } from 'expo-image';
 
-export default function SplashScreen() {
+export default function SplashScreen({ onFinish }: { onFinish: () => void }) {
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(onFinish);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
       <Image
         source={require('../assets/images/NoDoze.svg')}
         style={styles.logo}
         contentFit="contain"
       />
-    </View>
+    </Animated.View>
   );
 }
 
